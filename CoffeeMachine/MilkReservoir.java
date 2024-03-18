44438ef53d5753122b3c2fa90cb62c712fcb44cb
NovaS1de <148364873+NovaS1de@users.noreply.github.com>
2024-03-18 20:57:36 +0400

    БЕБЕБЕБЕ

--- CoffeeMachine/MilkReservoir.java
+++ CoffeeMachine/MilkReservoir.java
@@ -1,6 +1,15 @@
 public class MilkReservoir {
     boolean milkIsNeeded;
     int amountOfMilk;
+    int volumeOfTheReservoir;
+    public void pourMilkIntoTheReservoir(int amountOfMilkToPourIntoTheReservoir){
+        if(volumeOfTheReservoir<=this.amountOfMilk+amountOfMilkToPourIntoTheReservoir){
+            System.out.println("Ты пролил "+(amountOfMilk+amountOfMilkToPourIntoTheReservoir-volumeOfTheReservoir)+" мл. молока, грёбанный психопат!!");
+            amountOfMilk = volumeOfTheReservoir;
+        }else {
+            amountOfMilk = amountOfMilk + amountOfMilkToPourIntoTheReservoir;
+        }
+    }
     public void pourMilk(boolean milkIsNeeded, int amountOfMilk){
         if(milkIsNeeded){
             System.out.println("Наливаю "+amountOfMilk+" мл. молока");
