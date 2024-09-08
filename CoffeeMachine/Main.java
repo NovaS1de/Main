cbdb27f92c4f14bdcd8fd73e85424874f6b444a1
NovaS1de <148364873+NovaS1de@users.noreply.github.com>
2024-09-08 12:19:22 +0400

    Update Main.java

--- CoffeeMachine/Main.java
+++ CoffeeMachine/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        CoffeeMachine EBOSCH2000SUK = new CoffeeMachine(500, 200, 1500, 300, 100, 80);
-        Latte chashechkaLatte = EBOSCH2000SUK.latte();
-        Americano chashechkaAmericano = EBOSCH2000SUK.americano();
-        Espresso chashechkaEspresso = EBOSCH2000SUK.espresso();
-        Capuccino chashechkaCapuccino = EBOSCH2000SUK.capuccino();
+        CoffeeMachine LAMBORGHINI2000SVJ = new CoffeeMachine(500, 200, 1500, 300, 100, 80);
+        Latte chashechkaLatte = LAMBORGHINI2000SVJ.latte();
+        Americano chashechkaAmericano = LAMBORGHINI2000SVJ.americano();
+        Espresso chashechkaEspresso = LAMBORGHINI2000SVJ.espresso();
+        Capuccino chashechkaCapuccino = LAMBORGHINI2000SVJ.capuccino();
     }
 }
